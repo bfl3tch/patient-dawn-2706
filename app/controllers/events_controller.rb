a55090177ffda2168020c2937cd7e418dfed91d9
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-04 15:13:16 -0400

    Clean up

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,7 @@
 class EventsController < ApplicationController
   def create
-    # require "pry"; binding.pry
     competition = Competition.find(params[:id])
     team = competition.teams.create!(nickname: params[:nickname], hometown: params[:hometown])
-    # Maintenance.create!(mechanic_id: mechanic.id, ride_id: params[:ride_id])
-    # mechanic.maintenances.create!(ride_id: ride)
-    # ride.maintenances.create!(mechanic: mechanic)
-    # competition.teams << team
     redirect_to "/competitions/#{competition.id}"
   end
 end
--- app/models/competition.rb
+++ app/models/competition.rb
@@ -3,6 +3,6 @@ class Competition < ApplicationRecord
   has_many :teams, through: :events
 
   def average_age_of_players
-      teams.joins(:players).select('players.*').average(:age).to_f
-    end
+    teams.joins(:players).select('players.*').average(:age).to_f
+  end
 end
